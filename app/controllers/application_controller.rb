b9b5d8d2c0129e6c7cdf0ae97d1c4ed0f7aefcd7
ryo-arakawa <arkw.bump@gmail.com>
2022-05-26 15:29:41 +0900

    initial commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,20 @@
+class ApplicationController < ActionController::Base
+  before_action :authenticate_user!, except: [:top, :about]
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def after_sign_in_path_for(resources)
+    user_path(resources)
+  end
+
+  def after_sign_out_path_for(resources)
+    root_path
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :profile_image])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :introduction])
+  end
+end
